a68574263205076c9673d9a7d4c55b521116986a
Suraj Subraveti <suraj.vhb@gmail.com>
2017-12-09 15:45:11 -0500

    Added more metrics to callback.

--- src/baseline/LogCallbacks.py
+++ src/baseline/LogCallbacks.py
@@ -8,8 +8,11 @@ class Logger(Callback):
     def __init__(self, validation_data):
         super(Logger, self).__init__()
         self.X_val, self.y_true = validation_data
-        self.metric_array = []
+        self.metric1_array = []
+        self.metric2_array = []
         self.jaccard_similarity = []
+        self.train_loss = []
+        self.val_loss = []
 
     def eval_jaccard_similarity(self):
 
@@ -22,23 +25,35 @@ class Logger(Callback):
         return numerator/denominator
 
 
-    def eval_metric(self):
+    def eval_metrics(self):
         X_val = self.X_val
         y_true = self.y_true
         y_pred = self.model.predict(X_val)
         y_true_cols = np.count_nonzero(y_true, axis=1)
         correct_pred = 0
+        sum_metric2 = 0.0
         for i in xrange(len(y_pred)):
+            print y_true_cols[i]
             pred_indices = np.argsort(y_pred[i])[-y_true_cols[i]:][::-1]
             true_indices = np.argsort(y_true[i])[-y_true_cols[i]:][::-1]
-            if len(np.intersect1d(pred_indices, true_indices)) > 0:
+            intersect =  np.intersect1d(pred_indices, true_indices)
+            if len(intersect) > 0:
                 correct_pred += 1
-        return float(correct_pred) / len(X_val)
+            #sum_metric2+=float(len(intersect))/y_true_cols[i]
+        return float(correct_pred) / len(X_val), sum_metric2/len(y_true)
 
+    def on_batch_end(self, batch, logs={}):
+        print "Logging train loss!"
+        self.train_loss.append(logs['loss'])
+    
     def on_epoch_end(self, epoch, logs={}):
-        score = self.eval_metric()
+        
+        score1, score2 = self.eval_metrics()
         jaccard = self.eval_jaccard_similarity()
-        print "\n Accuracy for epoch %d is %f" % (epoch, score)
-        print "\n Jaccard similarity for epoch %d is %f" % (epoch, score)
-        self.metric_array.append(score)
+        print "\n Accuracy1 for epoch %d is %f" % (epoch, score1)
+        print "\n Accuracy2 for epoch %d is %f" % (epoch, score2)
+        print "\n Jaccard similarity for epoch %d is %f" % (epoch, jaccard)
+        
+        self.metric1_array.append(score1)
+        self.metric2_array.append(score2)
         self.jaccard_similarity.append(jaccard)
--- src/baseline/fc_net.py
+++ src/baseline/fc_net.py
@@ -1,10 +1,12 @@
 import cPickle as p
 
+import matplotlib.pyplot as plt
 from LogCallbacks import Logger
 import numpy as np
 import keras.backend as K
 import numpy as np
 import pandas as pd
+import seaborn as sns
 from keras.layers import Dense, Dropout, Activation
 from keras.models import Sequential
 from keras.optimizers import Adam
@@ -26,22 +28,6 @@ def jaccard_similarity(y_true, y_pred):
     return -(2 * K.sum(y_int) / (K.sum(y_true) + K.sum(y_pred)))
 
 
-def mutli_accuracy(y_true, y_pred):
-    print type(K.eval(y_pred))
-    '''
-    y_true_cols = np.count_nonzero(y_true, axis=1)
-    correct_pred = 0
-    for i in xrange(len(y_pred)):
-        pred_indices = np.argsort(y_pred[i])[-y_true_cols[i]:][::-1]
-        true_indices = np.argsort(y_true[i])[-y_true_cols[i]:][::-1]
-        if len(np.intersect1d(pred_indices, true_indices)) > 0:
-            correct_pred += 1
-
-    return K.tf.convert_to_tensor(float(correct_pred) / len(y_pred), np.float32)
-    '''
-    return y_pred
-
-
 def load_data_and_transform(lda_dump, data_dump):
     df = p.load(open(data_dump, 'rb'))
     data_frame = p.load(open(lda_dump, 'rb'))
@@ -53,7 +39,6 @@ def get_train_val_test(lda_dump, dump):
     X, y = load_data_and_transform(lda_dump=lda_dump, data_dump=dump)
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=1)
     return X_train, X_test, y_train, y_test
-
 
 def run_model(X_train, y_train):
     X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, test_size=0.2, random_state=1)
@@ -89,8 +74,18 @@ def run_model(X_train, y_train):
     print("[INFO] compiling model...")
     adam = Adam(lr=5e-3)
     model.compile(loss='binary_crossentropy', optimizer=adam, metrics=[jaccard_similarity])
-    model.fit(X_train, y_train, validation_data=(X_val, y_val), epochs=35, batch_size=128, callbacks=[logger])
-    return model
+    history = model.fit(X_train, y_train, validation_data=(X_val, y_val), epochs=35, batch_size=128, callbacks=[logger])
+
+    return model, logger, history
+
+def plot_loss(logger):
+
+    train_loss =  logger.train_loss
+    #val_loss = logger.val_loss
+    x_axis = range(len(train_loss))
+    sns.tsplot(data=train_loss, time=x_axis, value='Loss',legend = True)
+    #sns.tsplot(data=val_loss, tim=x_axis, value='Loss',legend = True)
+    plt.show()
 
 
 def predict(model, X_val, y_true):
@@ -109,7 +104,8 @@ def predict(model, X_val, y_true):
 if __name__ == '__main__':
     X_train, X_test, y_train, y_test, = get_train_val_test(LDA_DUMP, DUMP)
     print X_train.shape
-    fc_net_model = run_model(X_train, pd.DataFrame.as_matrix(y_train))
-    results = predict(fc_net_model, X_test, y_test.as_matrix())
-    print(results)
+    fc_net_model, logger, history = run_model(X_train, pd.DataFrame.as_matrix(y_train))
+    plot_loss(logger)
+    #results = predict(fc_net_model, X_test, y_test.as_matrix())
+    #print(results)
     # print results
